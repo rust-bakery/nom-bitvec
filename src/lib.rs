b734bf2a7f9484418efbed1e56e94e85e120fcf2
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-07-14 21:41:59 +0200

    start integrating

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+use bitvec::prelude::*;
+
+mod input;
+
+#[derive(Debug,PartialEq,Eq)]
+#[repr(transparent)]
+pub struct BSlice<'a, O: BitOrder, T: BitStore>(pub &'a BitSlice<O, T>);
+pub struct BArray<O: BitOrder, T: BitStore>(pub BitArray<O, T>);
